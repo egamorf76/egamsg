f8305d58afb85850b90b7c978ad26e0c8170e3f1
egamorf76 <maxime.adler76@gmail.com>
2022-07-05 17:55:12 +0200

    add static path

--- src/server.js
+++ src/server.js
@@ -1,6 +1,10 @@
 const express = require("express");
 const app = express();
 const port = process.env.PORT || 3000;
+const staticPath = path.join(__dirname, "../public");
+
+app.use(express.static(staticPath))
+
 
 app.get("/", function (req, res) {
     res.send("test");
