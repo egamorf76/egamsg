42ce84ed2b9fd433ca862912479d0fcf47d25c71
egamorf76 <maxime.adler76@gmail.com>
2022-07-06 17:22:11 +0200

    add web socket fonction

--- src/server.js
+++ src/server.js
@@ -1,15 +1,38 @@
 const express = require("express");
+const WebSocket = require("ws");
+
+const wss = new WebSocket.Server({ noServer: true });
 const app = express();
 const port = process.env.PORT || 3000;
 const staticPath = path.join(__dirname, "../public");
 
-app.use(express.static(staticPath))
+wss.on('open', function (ws) {
+    ws.send('Hello world');
+})
 
+wss.on('message', function (data) {
+    console.log(`Received: ${data}`)
+})
+
+app.use(express.static(staticPath))
 
 app.get("/", function (req, res) {
     res.send("test");
 })
 
-app.listen(port, function () {
+const server = app.listen(port, function () {
     console.log(`Server run on port ${port}`)
 })
+
+server.on('upgrade', function (req, socket, head) {
+    const {pathname} = parse(req.url);
+
+    if (pathname === '/wss') {
+        wss.handleUpgrade(req, socket, head, function (ws) {
+            wss.emit('connection', ws, req);
+        });
+    } else {
+        socket.destroy();
+    }
+})
+
